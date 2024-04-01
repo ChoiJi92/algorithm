654bcfd4dfaef9b650079ed2cb4fbd2ef5744fcd
Choiji92 <jeahoon100@naver.com>
2024-04-01 20:05:17 +0900

    first commit

---
+++ Backjoon/back1000.js
@@ -0,0 +1,6 @@
+let fs = require('fs');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
+
+const a = Number(input[0])
+const b = Number(input[1])
+console.log(a + b)

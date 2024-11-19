c4685d3c234f64a05450113f2306597d747b25c1
Jihoon <103625778+ChoiJi92@users.noreply.github.com>
2024-11-19 13:04:05 +0900

    [Silver I] Title: 정수 삼각형, Time: 180 ms, Memory: 14596 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,33 @@
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
+
+const N = +input.shift();
+
+const array = input.map((v) => v.split(" ").map(Number));
+
+if (N === 1) return console.log(array[0][0]);
+
+array[1][0] = array[0][0] + array[1][0];
+array[1][1] = array[0][0] + array[1][1];
+
+if (N === 2) return console.log(Math.max(array[1][0], array[1][1]));
+
+for (let i = 2; i < N; i++) {
+  for (let j = 0; j < array[i].length; j++) {
+    if (j === 0 || j === array[i].length - 1) {
+      if (j === 0) {
+        array[i][j] = array[i - 1][j] + array[i][j];
+      } else {
+        array[i][j] = array[i - 1][j - 1] + array[i][j];
+      }
+    } else {
+      array[i][j] =
+        Math.max(array[i - 1][j], array[i - 1][j - 1]) + array[i][j];
+    }
+  }
+}
+
+console.log(Math.max(...array[N - 1]));
